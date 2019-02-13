dca3635fb638065a2eef9bd9cac412ea477e8b55
Melinda Mezinca <melinda.mezinca@yahoo.com>
2019-02-13 17:16:27 +0200

    Adds initial connection to local mongo db

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const app = require('express')();
 const bodyParser = require('body-parser'); //package used to handle JSON requests in a middleware
-const port = process.env.port || 5000;
+const constants = require('./src/utils/constants.js');
+const dbUrl = constants.db.url;
+const port = process.env.port || constants.server.port;
+const dbConfig = require('./src/db/config.js');
+dbConfig.connection(dbUrl).then(() =>  console.log('connection succesful'))
 
+.catch((err) => console.error(err));
 //body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
---
+++ src/db/config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+//Use mongoose with native Node Promise.
+mongoose.Promise = global.Promise;
+//'mongodb://localhost:27017/default'
+//Create a connection to MongoDB.
+const connect = (dbUrl) => {
+return mongoose.connect(dbUrl,  { useNewUrlParser: true })
+}
+
+module.exports = {connection: connect};
